80d6624d4e40cbcd7261685b0697631c6877d331
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-07 00:56:55 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import hw4 from '../../HW-4-Arrays/main';
+import hw6 from '../../HW6-Objects/main';
+import hw8 from '../../HW8-Classes/main';
+import hw9 from '../../HW-9/main';
+import hw10 from '../../Home-work10-Events/main';
+import '../../Home-work10-Events/style.css';
+import hw11 from '../../HW-11-promise/main';
+import hw12 from '../../HW13-Generator/main';
