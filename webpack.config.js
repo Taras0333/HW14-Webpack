85ab6cbef5501e95e7ffa4aae30c6c0e17f8e69c
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-07 02:28:57 +0300

    2

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    mode: "development",
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        contentBase: './',
+        hot: true
+    },
+    plugins: [new HtmlWebpackPlugin({template: "./index.html"}), new MiniCssExtractPlugin()],
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                 ],
             },
